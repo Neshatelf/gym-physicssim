23d15923c21dfd617ef2816ffbb530b0682155a6
Neshatelf <39158626+Neshatelf@users.noreply.github.com>
2019-11-15 17:17:16 -0500

    Update __init__.py

--- gym_physicssim/__init__.py
+++ gym_physicssim/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
  
 register(id='physicssim-v0', 
-entry_point='gym.envs.gym_physicssim.envs:PhysicsSimEnv', 
+entry_point='gym.envs.gym_physicssim:PhysicsSimEnv', 
 kwargs={ 'draw_tiles': 1},
 reward_threshold=(3266 - 40),
 nondeterministic=True,
