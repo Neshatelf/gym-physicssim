a6fc65bd719fa84986543823de3b2d4bfd62d802
Neshatelf <39158626+Neshatelf@users.noreply.github.com>
2019-11-16 13:46:58 -0500

    Update __init__.py

--- gym_physicssim/envs/__init__.py
+++ gym_physicssim/envs/__init__.py
@@ -1,1 +1,11 @@
 from gym_physicssim.envs.physicssim_env import PhysicsSimEnv
+
+from gym.envs.registration import register
+ 
+register(id='physicssim-v0', 
+entry_point='gym.envs.gym_physicssim:PhysicsSimEnv', 
+)
+
+
+
+
