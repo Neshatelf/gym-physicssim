a4942570f65701ea357a862882c15abd722d96f5
Neshatelf <39158626+Neshatelf@users.noreply.github.com>
2019-11-18 12:21:14 -0500

    Update __init__.py

--- gym_physicssim/__init__.py
+++ gym_physicssim/__init__.py
@@ -5,8 +5,5 @@ logger = logging.getLogger(__name__)
  
 register(
     id='Physicssim-v0', 
-    entry_point='gym_physicssim.envs:PhysicssimEnv', 
-    timestep_limit=1000,
-    reward_threshold=1.0,
-    nondeterministic = True,         
+    entry_point='gym_physicssim.envs:PhysicssimEnv',          
 )
