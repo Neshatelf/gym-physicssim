d0c0e67e04848380dfb162032bac92e7f02c9eb7
Neshatelf <39158626+Neshatelf@users.noreply.github.com>
2019-11-18 11:22:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
  
 setup(name='gym_physicssim',
-      version='0.01',
-      install_requires=['gym', 'numpy', 'pygame']
+      version='0.0.1',
+      install_requires=['gym>=0.2.3',
+                        'hfo_py>=0.2',
+                        'numpy',
+                        'pygame']
 )
